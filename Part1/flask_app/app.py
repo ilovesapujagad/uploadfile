13da8252ef5f31423d312c3b868bd596ace50e20
lovesapujagad <118962746+ilovesapujagad@users.noreply.github.com>
2023-06-23 09:48:54 +0700

    Update app.py

--- Part1/flask_app/app.py
+++ Part1/flask_app/app.py
@@ -7,15 +7,6 @@ app = Flask(__name__)
 simple_app = Celery('tasks', broker='redis://10.10.66.193:6379/0', backend='redis://10.10.66.193:6379/0')
 
 
-@app.route('/simple_start_task')
-def call_method():
-    app.logger.info("Invoking Method ")
-    #                        queue name in task folder.function name
-    r = simple_app.send_task('tasks.longtime_add', kwargs={'x': 1, 'y': 2})
-    
-    app.logger.info(r.backend)
-    return r.id
-
 @app.route('/analyze_file', methods=['POST'])
 def analyze_file():
     if 'file' not in request.files:
@@ -24,17 +15,29 @@ def analyze_file():
     file = request.files['file']
     if file.filename == '':
         return 'No file selected', 400
-    indexname = request.form.get('indexname')
+    bulan = request.form.get('bulan')
+    if bulan == '':
+        return 'No bulan selected', 400
+    tahun = request.form.get('tahun')
+    if tahun == '':
+        return 'No tahun selected', 400
+    jenis = request.form.get('jenis')
+    if jenis == '':
+        return 'No jenis selected', 400
+    typename = request.form.get('typename')
+    if typename == '':
+        return 'No typename selected', 400
+    indexname = jenis+'-'+bulan+'-'+tahun+'-'+typename
     file_data = file.read()
     r = simple_app.send_task('tasks.process_task', kwargs={'file': file_data, 'indexname': indexname})
     app.logger.info(r.backend)
-    return r.id
+    return jsonify({'status':"SUCCES",'id':r.id})
 
 @app.route('/task_status/<task_id>')
 def get_status(task_id):
     status = simple_app.AsyncResult(task_id, app=simple_app)
     print("Invoking Method ")
-    return "Status of the Task " + str(status.state)
+    return jsonify({'status':str(status.state)})
 
 @app.route('/cancel_upload/<task_id>')
 def cancel_upload(task_id):
@@ -43,7 +46,7 @@ def cancel_upload(task_id):
     if result.state == 'PENDING':
         # Hentikan tugas yang sedang berjalan
         result.revoke(terminate=True)
-        return f"Upload task with ID {task_id} has been canceled."
+        return jsonify({'status':f"Upload task with ID {task_id} has been canceled."})
     
     return f"Cannot cancel task with ID {task_id}. Task is not in PENDING state."
 
@@ -51,7 +54,8 @@ def cancel_upload(task_id):
 @app.route('/task_result/<task_id>')
 def task_result(task_id):
     result = simple_app.AsyncResult(task_id).result
-    return "Result of the Task " + str(result)
+    # return "Result of the Task " + str(result)
+    return jsonify({'status':str(result)})
 
 if __name__ == '__main__':
     app.run(debug=True)
